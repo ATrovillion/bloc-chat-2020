e22e34d3493338b37081a1fbcfba50331dd16be3
ATrovillion <aaron.trovillion@gmail.com>
2020-05-20 09:24:25 -0400

    Commit incomplete list chat rooms checkpoint work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.14.4/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA4M_N2iTMfRdbPdDRJgr7PjwCLgZ5fDrQ",
+    authDomain: "bloc-react-chat-2020.firebaseapp.com",
+    databaseURL: "https://bloc-react-chat-2020.firebaseio.com",
+    projectId: "bloc-react-chat-2020",
+    storageBucket: "bloc-react-chat-2020.appspot.com",
+    messagingSenderId: "1085700804788",
+    appId: "1:1085700804788:web:4e396eee3bf4e6ae1d07e1",
+    measurementId: "G-GNR1K5H5YS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
 
 function App() {
   return (
